353f7355a4f690d34a2ab1e4206486cdfbd06b63
bestminer12 <63381049+bestminer12@users.noreply.github.com>
2026-01-20 05:52:32 +0900

    Update variables.tf

--- public-exposure-SG/terraform/variables.tf
+++ public-exposure-SG/terraform/variables.tf
@@ -1,9 +1,3 @@
 variable "aws_region" {
   default = "ap-northeast-2"
 }
-
-variable "key_name" {
-  description = "EC2 SSH key pair name (optional)"
-  type        = string
-  default     = null
-}
